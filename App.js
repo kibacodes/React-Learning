f53c439700f5857fc00f86142ad6796ac5eb2cbf
Prashant Chakraborty <prashantchakraborty@Mac.attlocal.net>
2025-04-06 21:32:31 -0500

    React-Training-4-JSX-Learning-2

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const Title = () => (
-//   <h1 id="head" tabIndex="5">
-//     Namaste React using JSX 🚀
-//   </h1>
-// );
+const Title = () => (
+  <h1 id="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
 
 
 const title =  (
@@ -20,6 +20,8 @@ const title =  (
 const HeadingComponent = () => (
     <div id="container">
       {title}
+      {Title()}
+      {/* <Title /> */}
       {/* {data} */}
       <h1 className="heading">Namaste React Functional Component</h1>
     </div>
