8989ace1ebe48349bf4446d96b57f82063068759
Prashant Chakraborty <prashantchakraborty@Mac.attlocal.net>
2025-04-08 00:09:56 -0500

    React-Learning-5-Planning+Component-Building

--- App.js
+++ App.js
@@ -1,36 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 id="head" tabIndex="5">
-    Namaste React using JSX 🚀
-  </h1>
-);
+/* 
+* Header
+* - Logo
+* - Nav Items
+* Body
+* - Search
+* - RestaurantContainer
+*    - RestaurantCard
+*       - Image
+*       - Name of Restaurant, Star Ratings, cuisine, delivery time
+* Footer
+* - Copyright
+* - Links
+* - Address
+* - Contact
+*/
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" alt="logo-container" 
+                className="logo"/>
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <h3>Olive Garden</h3>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    );
+}
 
 
-const title =  (
-    <h1 id="head1" tabIndex="5">
-      Namaste React using JSX 🚀
-    </h1>
-  );
-
-//const data = api.getData();
-// JSX prevents Cross Site Scripting (XSS)
-
-const HeadingComponent = () => (
-    <div id="container">
-      {title}
-      {Title()}
-      {/* <Title /> */}
-      {/* {data} */}
-      <h1 className="heading">Namaste React Functional Component</h1>
-    </div>
-  );
-
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+            {/* // Footer */}
+        </div>
+    );
+}
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
