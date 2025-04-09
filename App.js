b4dd2b6fd628757ffdb6889b3fc17958c94f81aa
Prashant Chakraborty <prashantchakraborty@Mac.attlocal.net>
2025-04-09 01:37:33 -0500

    React-Learning-6.1

--- App.js
+++ App.js
@@ -1,75 +1,79 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* 
-* Header
-* - Logo
-* - Nav Items
-* Body
-* - Search
-* - RestaurantContainer
-*    - RestaurantCard
-*       - Image
-*       - Name of Restaurant, Star Ratings, cuisine, delivery time
-* Footer
-* - Copyright
-* - Links
-* - Address
-* - Contact
-*/
+/*
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - RestaurantContainer
+ *    - RestaurantCard
+ *       - Image
+ *       - Name of Restaurant, Star Ratings, cuisine, delivery time
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" alt="logo-container" 
-                className="logo"/>
-            </div>
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+          alt="logo-container"
+          className="logo"
+        />
+      </div>
 
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const RestaurantCard = () => {
-    return (
-        <div className="res-card">
-            <h3>Olive Garden</h3>
-        </div>
-    );
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        src="https://img.cdn4dd.com/p/fit=cover,width=150,height=150,format=auto,quality=90/media/photosV2/2f662f18-dfdb-473c-8d76-ee9e7edad488-retina-large.jpg"
+        alt="res-logo"
+        className="res-logo"
+      />
+      <h3>Banda Burrito</h3>
+    </div>
+  );
 };
 
 const Body = () => {
-    return (
-        <div className="body">
-            <div className="search">Search</div>
-            <div className="res-container">
-                <RestaurantCard />
-            </div>
-        </div>
-    );
-}
-
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
-    return (
-        <div className="app">
-            <Header />
-            <Body />
-            {/* // Footer */}
-        </div>
-    );
-}
-
-
-
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      {/* // Footer */}
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
